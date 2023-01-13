cd528ac3fcade03661e5c5e83097f4bf26969eb3
Robin Witt <robin.w@posteo.de>
2023-01-13 14:27:55 +0100

    mapping from db

--- components/entryCard/index.js
+++ components/entryCard/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export default function EntryCard({ key, image, name, location }) {
+export default function EntryCard({ image, name, location }) {
   return (
-    <li key={key}>
+    <li>
       <Image src={image} alt={name} width={240} height={240} />
       <p>{name}</p>
       <p>{location}</p>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
-  images: { domains: ["images.unplash.com"] },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
